110bff700420cee22d4488b3ca2aabdab0f91660
Marcos Brendon De Paula <marcos.brendon@ufms.br>
2022-03-22 14:19:00 -0300

    modularização do app

--- main.js
+++ main.js
@@ -1,29 +1,13 @@
-const express = require('express');
-const { engine } = require ('express-handlebars');
-const bodyParser = require('body-parser');
-const logger = require('morgan');
-const path = require('path');
-const app = express();
+const app = require('./app');
+
+// Routes
+require('./routes/routes')(app)
 
 const PORT = process.env.PORT || 3000;
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 app.set('port', PORT);
 app.set('env', NODE_ENV);
-
-app.use(logger('tiny'));
-app.use(bodyParser.json());
-
-// Handlebars
-app.engine('handlebars', engine())
-app.set('view engine', 'handlebars')
-
-// Public
-app.use(express.static(path.join(__dirname, 'public')))
-
-// Routes
-require('./routes/routes')(app)
-
 
 app.listen(PORT, () => {
   console.log(
