759eeb6e4a872922e1cafd86115097a775ea0b3a
Marcos Brendon De Paula <marcos.brendon@ufms.br>
2022-03-22 11:35:55 -0300

    Informaçoes

---
+++ routes/audio.js
@@ -0,0 +1,7 @@
+const Express = require('express')
+const Router = Express.Router()
+
+//https://www.npmjs.com/package/youtube-mp3-downloader
+//https://www.npmjs.com/package/node-youtube-music
+
+module.exports = Router
---
+++ routes/video.js
@@ -0,0 +1,7 @@
+const Express = require('express')
+const Router = Express.Router()
+
+//https://www.npmjs.com/package/youtube-dl - usado no video https://www.youtube.com/watch?v=ImUzmucwSPI
+//https://www.npmjs.com/package/youtube-dl-exec
+
+module.exports = Router
