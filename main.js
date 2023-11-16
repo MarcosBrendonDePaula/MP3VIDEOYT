5c876e60fb1457a7e3d958a7515a81577d1ec627
Marcos Brendon De Paula <marcos.brendon@ufms.br>
2023-11-16 19:45:36 -0300

    atualização de projeto

--- main.js
+++ main.js
@@ -2,55 +2,53 @@ const cluster = require('cluster')
 const os = require('os')
 const app = require('./app');
 
-async function forceLoad(){
-  while (true) {
-    try{
-      await require('ytdl-core').getInfo("41tWZlh4SP8")
-      return;
-    } catch(err) {
-      console.log(err)
-      process.exit()
-    }
-  }
-}
+// async function forceLoad(){
+//   while (true) {
+//     try{
+//       await require('ytdl-core').getInfo("41tWZlh4SP8")
+//       return;
+//     } catch(err) {
+//       console.log(err)
+//       process.exit()
+//     }
+//   }
+// }
 
 const numCpus = os.cpus().length; 
-require('./controller/Cache').Get()
+// require('./controller/Cache').Get()
 
-app.listen(app.get('port'),async () => {
-  await forceLoad()
-  console.log(
-    `worker ${process.pid} on Port ${app.get(
-      'port'
-    )} | Environment : ${app.get('env')}`
-  );
-});
+// app.listen(app.get('port'),async () => {
+//   await forceLoad()
+//   console.log(
+//     `worker ${process.pid} on Port ${app.get(
+//       'port'
+//     )} | Environment : ${app.get('env')}`
+//   );
+// });
 
-// if (cluster.isMaster){
+if (cluster.isMaster){
   
-//   require('./controller/Cache').Get()
+  require('./controller/Cache').Get()
 
-//   for(let i=0; i<numCpus; i++){ 
-//     cluster.fork()
-//   }
+  for(let i=0; i<numCpus; i++){ 
+    cluster.fork()
+  }
   
-//   cluster.on("exit", async (worker, code, signal) => {
-//     console.log(`worker ${worker.process.pid} died`);
-//     console.log("Let's fork another worker!");
-//     cluster.fork();
-//     await new Promise(r => setTimeout(r, 5000));
-//   });
+  cluster.on("exit", async (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
+    await new Promise(r => setTimeout(r, 5000));
+  });
 
-// } else {
-  
-//   console.log(`Worker ${process.pid} started`);
-
-//   app.listen(app.get('port'),async () => {
-//     await forceLoad()
-//     console.log(
-//       `worker ${process.pid} on Port ${app.get(
-//         'port'
-//       )} | Environment : ${app.get('env')}`
-//     );
-//   });
-// }
+} else {
+  console.log(`Worker ${process.pid} started`);
+  app.listen(app.get('port'),async () => {
+    // await forceLoad()
+    console.log(
+      `worker ${process.pid} on Port ${app.get(
+        'port'
+      )} | Environment : ${app.get('env')}`
+    );
+  });
+}
